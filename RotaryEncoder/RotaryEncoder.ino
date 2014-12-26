3679dcf49f07637ef31d16f9204d5ce02a16ac53
Bengt Marten Agren <marangisto@me.com>
2014-12-26 11:34:27 +0800

    Added RotaryEncoder project.

---
+++ RotaryEncoder/RotaryEncoder.ino
@@ -0,0 +1,37 @@
+/* Read Quadrature Encoder
+  * Connect Encoder to Pins encoder0PinA, encoder0PinB, and +5V.
+  *
+  * Sketch by max wolf / www.meso.net
+  * v. 0.1 - very basic functions - mw 20061220
+  *
+  */  
+
+
+ int val; 
+ int encoder0PinA = 3;
+ int encoder0PinB = 4;
+ int encoder0Pos = 0;
+ int encoder0PinALast = LOW;
+ int n = LOW;
+
+ void setup() { 
+   pinMode (encoder0PinA,INPUT);
+   pinMode (encoder0PinB,INPUT);
+   digitalWrite(encoder0PinA, HIGH);
+   digitalWrite(encoder0PinB, HIGH);
+   Serial.begin (9600);
+ } 
+
+ void loop() { 
+   n = digitalRead(encoder0PinA);
+   if ((encoder0PinALast == LOW) && (n == HIGH)) {
+     if (digitalRead(encoder0PinB) == LOW) {
+       encoder0Pos--;
+     } else {
+       encoder0Pos++;
+     }
+     Serial.print (encoder0Pos);
+     Serial.print ("/");
+   } 
+   encoder0PinALast = n;
+ }
