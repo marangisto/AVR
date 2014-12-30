7fa196b08af9a16dc1ff9565cfb5956d5226ac77
Bengt Marten Agren <marangisto@me.com>
2014-12-30 22:17:18 +0800

    Use ADXL336 in hacked instrument.

--- TrellisQuad/TrellisQuad.ino
+++ TrellisQuad/TrellisQuad.ino
@@ -1,18 +1,30 @@
 #include <Wire.h>
 #include "Adafruit_Trellis.h"
 #include "TrellisQuad.h"
+#include "AccelADXL335.h"
 
 void setup()
 {
 	Serial.begin(9600);
 	TrellisQuad::begin();
+	AccelADXL335::begin();
 }
 
 void loop()
 {
 	static int i = 0;
 
-	TrellisQuad::drawNum(1, 0, i);
+	AccelADXL335 accel;
+
+	Serial.print(accel.get_x());
+	Serial.print(" ");
+	Serial.print(accel.get_y());
+	Serial.print(" ");
+	Serial.print(accel.get_z());
+	Serial.println();
+
+//	TrellisQuad::drawNum(1, 0, i);
+	TrellisQuad::drawNum(1, 0, abs(accel.get_y()));
 
 	if (++i > 99)
 		i = 0;
