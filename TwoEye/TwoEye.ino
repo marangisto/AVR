eb2d931ace87eb4b6222ba281023be77f5e163e8
Bengt Marten Agren <marangisto@me.com>
2015-01-24 17:32:33 +0800

    First version of two-eye tracker.

--- TwoEye/TwoEye.ino
+++ TwoEye/TwoEye.ino
@@ -1,48 +1,61 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
- 
-  This example code is in the public domain.
- */
+#include <Servo.h>
+
 const int leftEye = A0;
 const int rightEye = A1;
 
 int leds[8] = { 5, 6, 7, 8, 9, 10, 11, 12 };
-int nLeds =  sizeof(leds) / sizeof(*leds);
+int nLeds =	sizeof(leds) / sizeof(*leds);
+
+static Servo g_servo;
+static int g_pos = 90;
+static int g_l0 = 0;
+static int g_r0 = 0;
 
 int EyeVal(int eyePin)
 {
-  int v = analogRead(eyePin);
-  return map(constrain(v, 25, 900), 25, 900, 1, 1000);
-}  
+	int v = analogRead(eyePin);
+	return map(constrain(v, 25, 900), 25, 900, 1, 1000);
+}	
 
-// the setup routine runs once when you press reset:
-void setup() {                
-  Serial.begin(9600);
-  // initialize the digital pin as an output.
-  for (int i = 0; i < nLeds; ++i)
-    pinMode(leds[i], OUTPUT);
-  //  pinMode(led, OUTPUT);     
+void setup() {								
+	Serial.begin(9600);
+	for (int i = 0; i < nLeds; ++i)
+		pinMode(leds[i], OUTPUT);
+	g_servo.attach(4);	// PIN4
+	g_servo.write(g_pos);
+	delay(1000);							 // wait for a second
+	g_l0 = EyeVal(leftEye);
+	g_r0 = EyeVal(rightEye);
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
-  static int i = 0;
-  
-  int l = EyeVal(leftEye);
-  int r = EyeVal(rightEye);
+	static int i = 0;
+	
+	int l = EyeVal(leftEye) - g_l0;
+	int r = EyeVal(rightEye) - g_r0;
 
-  Serial.println(l);
-  Serial.println(r);
-  
-  
-  
-  int j = i;
-  i = ++i % nLeds;
-  digitalWrite(leds[j], LOW);    // turn the LED off by making the voltage LOW
-  
-  digitalWrite(leds[i], HIGH);   // turn the LED on (HIGH is the voltage level)
-//  Serial.println(i);
-  delay(l);               // wait for a second
+	Serial.println(l);
+	Serial.println(r);
+	
+	int j = i;
+	i = ++i % nLeds;
+
+	digitalWrite(leds[j], LOW);		// turn the LED off by making the voltage LOW
+	digitalWrite(leds[i], HIGH);	 // turn the LED on (HIGH is the voltage level)
+
+	if (r > l)
+	{
+		if (g_pos < 180)
+			g_pos += 1;
+	}
+	else if (l > r)
+	{
+		if (g_pos > 0)
+			g_pos -= 1;
+	}
+
+	g_servo.write(g_pos);
+
+	delay(20);							 // wait for a second
 }
 
