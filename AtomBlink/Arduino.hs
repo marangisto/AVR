3d8db2671c845a013ebc6c60d257266540497240
Bengt Marten Agren <marangisto@me.com>
2015-05-13 00:06:43 +0800

    Added Gergo's quasi-quoted io code.

---
+++ AtomBlink/Arduino.hs
@@ -0,0 +1,59 @@
+{-# LANGUAGE QuasiQuotes #-}
+module Arduino (PinType(..), setupPins) where
+
+import qualified Data.Loc
+import qualified Data.Symbol
+import qualified Language.C.Syntax as C
+import Language.C.Quote.C
+
+data PinType = Input | Output
+
+instance ToIdent PinType where
+    toIdent Input = toIdent "INPUT"
+    toIdent Output = toIdent "OUTPUT"
+
+setupPins :: String
+          -> [(String, Int, PinType, String)]
+          -> ([C.Definition], [C.Definition], [String])
+setupPins atomName pins = (decls, defs, pinFuncNames)
+  where
+    decls = [cunit|
+
+             // OH GOD WHY?!
+             $esc:("#include <Arduino.h>")
+
+             $edecls:pinDecls
+             $edecls:pinFuncDecls
+            |]
+
+    defs = [cunit|
+            $edecl:setup
+            $edecls:pinFuncDefs
+            $edecl:loop
+           |]
+
+    pinDecls = [ [cedecl| typename uint16_t $id:pinName = $int:pinNum; |]
+               | (pinName, pinNum, _, _) <- pins
+               ]
+
+    pinFuncDecls = [ [cedecl| void $id:(toPinFunc pinName)(); |]
+                   | (pinName, _, pinType, _) <- pins
+                   ]
+
+    pinInit = [ [cstm| pinMode($id:pinName, $id:pinType); |]
+              | (pinName, _, pinType, _) <- pins
+              ]
+
+    pinFuncDefs = [ [cedecl| void $id:(toPinFunc pinName) () { $stm:updatePin } |]
+                  | (pinName, _, pinType, stateVar) <- pins
+                  , let updatePin = case pinType of
+                             Output -> [cstm| digitalWrite( $id:pinName, state.$id:atomName.$id:stateVar); |]
+                  ]
+
+    setup = [cedecl| void setup () { $stms:pinInit } |]
+    loop = [cedecl| void loop () { $id:atomName (); } |]
+
+    pinFuncNames = [ toPinFunc pinName | (pinName, _, _, _) <- pins ]
+
+    toPinFunc pinName = "update_" ++ pinName
+
--- AtomBlink/AtomBlink.hs
+++ AtomBlink/AtomBlink.hs
@@ -1,48 +1,30 @@
 module Blink (main) where
  
-import Language.Atom hiding (name)
+import Language.Atom
 import System.Directory (renameFile)
- 
-name :: Name
-name = "AtomBlink"
-    
-timeout :: Int
-timeout = 20000
- 
-blink :: Atom ()
-blink = do
-  on <- bool "on" True
-  period timeout $ atom "blinkOn" $ do
-    call "avr_blink"
-    on <== not_ (value on)
- 
-header :: String
-header = unlines [
-          "int16_t ledPin = 13;",
-          "void avr_blink(void);"
-         ]
- 
-footer :: String
-footer = unlines [
-          "void setup() {",
-          "  pinMode(ledPin, OUTPUT);",
-          "}",
-          "",
-          "void avr_blink() {",
-          "  digitalWrite(ledPin, state." ++ name ++ ".on);",
-          "}",
-          "",
-          "void loop() {",
-          "  " ++ name ++ "();",
-          "}"
-         ]
- 
-prePostCode :: [Name] -> [Name] -> [(Name, Type)] -> (String, String)
-prePostCode _ _ _ = (header, footer)
+import Text.PrettyPrint.Mainland
+import Arduino
  
 main :: IO ()
 main = do
-  (schedule, _, _, _, _) <- compile name defaults { cCode = prePostCode } blink
-  putStrLn $ reportSchedule schedule
-  renameFile (name ++ ".c") (name ++ ".ino") -- Makefile expects .ino
+    (schedule, _, _, _, _) <- compile name defaults { cCode = prePostCode } blink
+    putStrLn $ reportSchedule schedule
+    renameFile (name ++ ".c") (name ++ ".ino") -- Makefile expects .ino
+    where
+        name = "AtomBlink"
 
+        pins :: [(String, Int, PinType, String)]
+        pins = [("ledPin", 13, Output, "on")]
+
+        blink :: Atom ()
+        blink = do
+            on <- bool "on" True
+            period 40000 $ atom "blinkOn" $ do
+                call updateLED
+                on <== not_ (value on)
+ 
+        (decls, defs, [updateLED]) = setupPins name pins
+
+        prePostCode :: [Name] -> [Name] -> [(Name, Type)] -> (String, String)
+        prePostCode _ _ _ = (pretty 80 $ ppr decls, pretty 80 $ ppr defs)
+ 
