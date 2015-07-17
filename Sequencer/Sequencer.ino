9f7b9e6bb50a4464adce2904069f801fbebdef6c
Bengt Marten Agren <marangisto@me.com>
2015-07-17 20:16:09 +0800

    Experiment with sequencer extensions. Not working - may need to revert this commit.

--- Sequencer/Sequencer.ino
+++ Sequencer/Sequencer.ino
@@ -1,17 +1,31 @@
 #include <Wire.h>
-#include "TrellisQuad.h"
+//#include "TrellisQuad.h"
 // #include "AccelADXL335.h"
+
+static const uint8_t potGnd = 7;
+static const uint8_t potVcc = 5;
+static const uint8_t potOne = A6;
+static const uint8_t potTwo = A7;
+static const uint8_t led = 13;
 
 void setup()
 {
 	Serial.begin(9600);
-	TrellisQuad::begin();
+//	TrellisQuad::begin();
 //	AccelADXL335::begin();
+
+	pinMode(potGnd, OUTPUT);
+	digitalWrite(potGnd, LOW);
+	pinMode(potVcc, OUTPUT);
+	digitalWrite(potVcc, HIGH);
+
+	pinMode(led, OUTPUT);
 }
 
 void loop()
 {
 	static int i = 0;
+	static bool on = false;
 
 /*
 	AccelADXL335 accel;
@@ -24,14 +38,20 @@ void loop()
 	Serial.println();
 */
 
-	TrellisQuad::drawNum(1, 0, i);
+   	i = map(analogRead(potTwo), 0, 1023, 0, 99);
+
+	Serial.println(i);
+//	TrellisQuad::drawNum(1, 0, i);
 //	TrellisQuad::drawNum(1, 0, abs(accel.get_y()));
 
 	if (++i > 99)
 		i = 0;
 
-	TrellisQuad::writeDisplay();
+//	TrellisQuad::writeDisplay();
 
-	delay(300);
+	digitalWrite(led, on);
+	on = !on;
+
+	delay(i);
 }
 
