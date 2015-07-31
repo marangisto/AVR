14384eee1616dd7833bc7c3b3bf125bf40990863
Bengt Marten Agren <marangisto@me.com>
2015-07-31 20:14:41 +0800

    Added MSB and LSB shift out order.

--- SN74HC595/Main.cpp
+++ SN74HC595/Main.cpp
@@ -1,15 +1,40 @@
 #include "../AVR/Pins.h"
 #include "../AVR/Delay.h"
 
+enum shift_order_t { LSB_FIRST = 0, MSB_FIRST = 1};
+
 template<class DT, class CK>
+static inline void clock_out_bit(uint8_t x, int i)
+{
+	clear<CK>();
+	write<DT>(((x >> i) & 0x01) != 0);
+	set<CK>();
+}
+
+template<class DT, class CK, shift_order_t SD>
+struct shift_out_impl
+{
+	static void shift_out(uint8_t x)
+	{
+		for (int8_t i = 7; i >= 0; --i)
+			clock_out_bit<DT, CK>(x, i);
+	}
+};
+
+template<class DT, class CK>
+struct shift_out_impl<DT, CK, LSB_FIRST>
+{
+	static void shift_out(uint8_t x)
+	{
+		for (int8_t i = 0; i < 8; ++i)
+			clock_out_bit<DT, CK>(x, i);
+	}
+};
+
+template<class DT, class CK, shift_order_t SD>
 void shift_out(uint8_t x)
 {
-	for (int8_t i = 7; i >= 0; --i)
-	{
-		clear<CK>();
-		write<DT>(((x >> i) & 0x01) != 0);
-		set<CK>();
-	}
+	shift_out_impl<DT, CK, SD>::shift_out(x);
 }
 
 typedef pin_t<PB, 3> LED;
@@ -30,7 +55,7 @@ void loop()
 		toggle<LED>();
 		delay(1);
 		clear<LATCH>();
-		shift_out<DATA, CLOCK>(i);
+		shift_out<DATA, CLOCK, MSB_FIRST>(i);
 		set<LATCH>();
 	}
 }
