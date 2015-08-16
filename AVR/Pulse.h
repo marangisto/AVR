0f9e2919382468d5678040515ad7bff3374e9658
Bengt Marten Agren <marangisto@me.com>
2015-08-16 15:02:10 +0800

    Adjusted loop cost for pulse_width.

--- AVR/Pulse.h
+++ AVR/Pulse.h
@@ -7,21 +7,22 @@
 template<class PIN, bool PULSE_STATE>
 uint32_t pulse_width(uint32_t timeout_us)
 {
+	const uint8_t x = PULSE_STATE ? PIN::mask : 0;
     uint32_t w = 0, i = 0, n = clocks_per_us * timeout_us / 16;
 
-    while (read<PIN>() == PULSE_STATE)
+    while ((PIN::rpin() & PIN::mask) == x)
         if (++i > n)
             return 0;
 
-    while (read<PIN>() != PULSE_STATE)
+    while ((PIN::rpin() & PIN::mask) != x)
         if (++i > n)
             return 0;
 
-    while (read<PIN>() == PULSE_STATE)
+    while ((PIN::rpin() & PIN::mask) == x)
         if (++w, ++i > n)
             return 0;
 
-    return (w * 10 + 16) / clocks_per_us;
+    return (w * 17 + 16) / clocks_per_us;
 }
 
 #endif PULSE_H
