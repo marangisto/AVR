52d9fab4432be122a1f7e1f7e25b3135e8b6b47f
Bengt Marten Agren <marangisto@me.com>
2015-08-27 23:07:13 +0800

    Fixed 30 second interval timer.

---
+++ OneDX/Main.cpp
@@ -0,0 +1,35 @@
+#include "../AVR/Pins.h"
+#include "../AVR/Delay.h"
+
+typedef pin_t<PB, 0> FOCUS;
+typedef pin_t<PB, 1> EXPOSE;
+
+void shoot()
+{
+	set<FOCUS>();
+	delay_ms(100);
+	set<EXPOSE>();
+	delay_ms(100);
+	clear<EXPOSE>();
+	clear<FOCUS>();
+}
+
+void setup()
+{
+	digital_out<FOCUS>();
+	digital_out<EXPOSE>();
+}
+
+void loop()
+{
+	shoot();
+	delay_ms(30000);
+}
+
+int main()
+{
+	setup();
+	for (;;)
+		loop();
+}
+
