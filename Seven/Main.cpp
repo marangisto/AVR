1fd0b974c5f0b929962bd4b9d6c40a77ce0092bb
Bengt Marten Agren <marangisto@me.com>
2015-08-30 15:44:09 +0800

    Cleaned up initial 7-segment driver.

--- Seven/Main.cpp
+++ Seven/Main.cpp
@@ -14,9 +14,10 @@ typedef pin_t<PD, 1> D;
 typedef pin_t<PD, 0> E;
 typedef pin_t<PC, 3> F;
 typedef pin_t<PD, 6> G;
-typedef pin_t<PD, 2> DP;
+typedef pin_t<PD, 2> H;
 
-typedef bits_t<A, B, C, D, E, F, G> digit_t;
+typedef bits_t<D0, D1, D2, D3> digit_t;
+typedef bits_t<A, B, C, D, E, F, G, H> segment_t;
 
 void setup()
 {
@@ -25,10 +26,7 @@ void setup()
 	digital_out<D2>();
 	digital_out<D3>();
 
-	set<D0>();
-	set<D1>();
-	set<D2>();
-	set<D3>();
+	write_bits<digit_t>(~0);
 
 	digital_out<A>();
 	digital_out<B>();
@@ -37,27 +35,9 @@ void setup()
 	digital_out<E>();
 	digital_out<F>();
 	digital_out<G>();
-	digital_out<DP>();
+	digital_out<H>();
 
-	set<A>();
-	set<B>();
-	set<C>();
-	set<D>();
-	set<E>();
-	set<F>();
-	set<G>();
-	set<DP>();
-}
-
-static void display(uint8_t x)
-{
-	write<A>((x & (1 << 0)) == 0);
-	write<B>((x & (1 << 1)) == 0);
-	write<C>((x & (1 << 2)) == 0);
-	write<D>((x & (1 << 3)) == 0);
-	write<E>((x & (1 << 4)) == 0);
-	write<F>((x & (1 << 5)) == 0);
-	write<G>((x & (1 << 6)) == 0);
+	write_bits<segment_t>(~0);
 }
 
 void loop()
@@ -65,42 +45,27 @@ void loop()
 	static uint16_t i = 0;
 	static uint8_t j = 0, k = 0;
 	static char chars[4] = { '4', '5', '6', '7' };
+	static const uint8_t nchar = sizeof(chars) / sizeof(*chars);
 
-	if ((++j & 0xf) == 0)
+	if ((++j & 0x7f) == 0)
 	{
-		chars[3] = '0' + i % 10;
-		chars[2] = '0' + (i / 10) % 10;
-		chars[1] = '0' + (i / 100) % 10;
-		chars[0] = '0' + (i / 1000) % 10;
+		uint16_t a = i;
+
+		for (uint8_t c = 0; c < nchar; ++c)
+		{
+			chars[nchar - c - 1] = '0' + a % 10;
+			a /= 10;
+		}
 
 		++i;
 	}
 
-	switch (k & 0x3)
-	{
-		case 0: set<D3>(); break;
-		case 1: set<D0>(); break;
-		case 2: set<D1>(); break;
-		case 3: set<D2>(); break;
-	}
+	write_bits<digit_t>(~0);
+	write_bits<segment_t>(~font_t::seg7(chars[k]));
+	write_bits<digit_t>(~(1 << k));
 
-	switch (k & 0x3)
-	{
-		case 0: write_bits<digit_t>(~font_t::seg7(chars[0])); break;
-		case 1: write_bits<digit_t>(~font_t::seg7(chars[1])); break;
-		case 2: write_bits<digit_t>(~font_t::seg7(chars[2])); break;
-		case 3: write_bits<digit_t>(~font_t::seg7(chars[3])); break;
-	}
-
-	switch (k & 0x3)
-	{
-		case 0: clear<D0>(); break;
-		case 1: clear<D1>(); break;
-		case 2: clear<D2>(); break;
-		case 3: clear<D3>(); break;
-	}
-
-	++k;
+	if (++k >= nchar)
+		k = 0;
 
 	delay_ms(1);
 }
