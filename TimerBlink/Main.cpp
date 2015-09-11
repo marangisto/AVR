13b80d9c4970fa9cadfd351a7647a4a81674597d
Bengt Marten Agren <marangisto@me.com>
2015-09-11 22:18:01 +0800

    Added timer based blink example.

---
+++ TimerBlink/Main.cpp
@@ -0,0 +1,40 @@
+#include <AVR/Pins.h>
+#include <AVR/Timer.h>
+#include <AVR/Delay.h>
+
+#if defined(__AVR_ATmega32U4__)
+typedef output_t<PC, 7> LED;			// leonardo
+#else
+typedef output_t<PB, 5> LED;			// uno
+#endif
+
+typedef timer_t<1> timer;
+
+static void isr()
+{
+	LED::toggle();
+}
+
+void setup()
+{
+	LED::setup();
+
+	timer::prescale(timer::prescale_64);
+	timer::isr(isr);
+	timer::enable();
+	sei();
+}
+
+void loop()
+{
+	// do something else
+	delay_ms(1000);
+}
+
+int main()
+{
+	setup();
+	for (;;)
+		loop();
+}
+
