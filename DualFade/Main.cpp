7bfdfe4901a15802a22916d1247fd6a35c750471
Bengt Marten Agren <maraingist@me.com>
2016-01-08 17:25:16 +0000

    Added six channels to dual fade demo.

--- DualFade/Main.cpp
+++ DualFade/Main.cpp
@@ -2,28 +2,46 @@
 #include <AVR/Timer.h>
 
 typedef timer_t<0> T;
+typedef timer_t<1> U;
+typedef timer_t<2> V;
 
 void setup()
 {
-    T::setup<pwm_phase_correct, top_0xff>();
+    const wg_mode mode = fast_pwm; // crashes non-deterinistically with pwm_phase_correct;
+
+    T::setup<mode, top_0xff>();
     T::clock_select<1>();
     T::output_pin<channel_a>::setup();
     T::output_pin<channel_b>::setup();
     T::compare_output_mode<channel_a, clear_on_compare_match>();
     T::compare_output_mode<channel_b, clear_on_compare_match>();
+
+    U::setup<mode, top_0xff>();
+    U::clock_select<1>();
+    U::output_pin<channel_a>::setup();
+    U::output_pin<channel_b>::setup();
+    U::compare_output_mode<channel_a, clear_on_compare_match>();
+    U::compare_output_mode<channel_b, clear_on_compare_match>();
+
+    V::setup<mode, top_0xff>();
+    V::clock_select<1>();
+    V::output_pin<channel_a>::setup();
+    V::output_pin<channel_b>::setup();
+    V::compare_output_mode<channel_a, clear_on_compare_match>();
+    V::compare_output_mode<channel_b, clear_on_compare_match>();
 }
 
 static unsigned duty_cycle(unsigned& i, bool& dir)
 {
-    static const int dcs[] = { 0, 0, 1, 1, 2, 2, 3, 4, 5, 7, 9, 12, 15
-                             , 19, 24 , 31, 39, 50, 63, 80, 101, 127, 160
-                             , 202, 254
-                             };
+    static const unsigned dcs[] = { 0, 0, 1, 1, 2, 2, 3, 4, 5, 7, 9, 12, 15
+                                  , 19, 24 , 31, 39, 50, 63, 80, 101, 127, 160
+                                  , 202, 254
+                                  };
 
     if (i == 0 || (i + 1) == sizeof(dcs) / sizeof(*dcs))
         dir = !dir;
 
-    int dc = dcs[i];
+    unsigned dc = dcs[i];
 
     i += dir ? 1 : -1;
     return dc;
@@ -32,14 +50,31 @@ static unsigned duty_cycle(unsigned& i, bool& dir)
 
 void loop()
 {
-    static unsigned i = 0, idx_a = 0, idx_b = 0;
-    static bool dir_a = false, dir_b = false;            // scan direction
+    static unsigned t_idx_a = 0, t_idx_b = 0;
+    static unsigned u_idx_a = 0, u_idx_b = 0;
+    static unsigned v_idx_a = 0, v_idx_b = 0;
+    static bool t_dir_a = false, t_dir_b = false;
+    static bool u_dir_a = false, u_dir_b = false;
+    static bool v_dir_a = false, v_dir_b = false;
+    static unsigned i = 0;
 
-    if (i % 21 == 0)
-        T::output_compare_register<channel_a>() = duty_cycle(idx_a, dir_a);
+    if (i % 13 == 0)
+        T::output_compare_register<channel_a>() = duty_cycle(t_idx_a, t_dir_a);
 
     if (i % 17 == 0)
-        T::output_compare_register<channel_b>() = duty_cycle(idx_b, dir_b);
+        T::output_compare_register<channel_b>() = duty_cycle(t_idx_b, t_dir_b);
+
+    if (i % 23 == 0)
+        U::output_compare_register<channel_a>() = duty_cycle(u_idx_a, u_dir_a);
+
+    if (i % 29 == 0)
+        U::output_compare_register<channel_b>() = duty_cycle(u_idx_b, u_dir_b);
+
+    if (i % 31 == 0)
+        V::output_compare_register<channel_a>() = duty_cycle(v_idx_a, v_dir_a);
+
+    if (i % 37 == 0)
+        V::output_compare_register<channel_b>() = duty_cycle(v_idx_b, v_dir_b);
 
     ++i;
 
