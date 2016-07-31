8d5d3b9d2b505be1cf489591370874298f8b3ef5
Bengt Marten Agren <maraingist@me.com>
2016-07-31 16:51:53 +0800

    Cosmetics.

--- TinyCTC/Tiny.hs
+++ TinyCTC/Tiny.hs
@@ -1,11 +1,9 @@
 module Main (main) where
 
 f_cpu = 8e6
-
-f n x = f_cpu / (2 * n * (1 + x))
-
-g n f = f_cpu / (f * 2 * n) - 1
+freq n c = f_cpu / (2 * n * (1 + c))
+count n f = f_cpu / (f * 2 * n) - 1
 
 main :: IO ()
-main = do
-    mapM_ (print . g 8) [ 2e3, 2.5e3, 3e3 ]
+main = mapM_ (print . round . count 8) [ 2e3, 2.5e3, 3e3 ]
+
