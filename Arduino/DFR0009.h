725f8b526ac237ad893759fc61e248d754a5b5f9
Bengt Marten Agren <maraingist@me.com>
2016-08-03 00:09:50 +0800

    Added button reading support to DFR0009.

--- Arduino/DFR0009.h
+++ Arduino/DFR0009.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <AVR/ADC.h>
 #include <AVR/LCD1602A.h>
 #include <Arduino/Pins.h>
 
@@ -28,4 +29,53 @@ private:
 };
 
 typedef tc1602_t<dfr0009_tc1602_t> dfr0009_t;
+
+enum button_t { btn_none, btn_release, btn_select, btn_left, btn_down, btn_up, btn_right };
+
+class buttons_t
+{
+public:
+	static void setup()
+	{
+		adc::setup<128>();
+	}
+
+    static button_t read()  // call every 1ms
+    {
+        static uint8_t count = 0;
+        static uint8_t last_state = 0;
+        uint8_t this_state = raw_read();
+
+        count = this_state != stable_state && this_state == last_state ? count + 1 : 0;
+        last_state = this_state;
+        if (count == stable_count)
+        {
+            stable_state = this_state;
+            return static_cast<button_t>(stable_state + 1);
+        }
+        else
+            return btn_none;
+    }
+
+private:
+    static const uint8_t CHANNEL = A0;
+    static const uint8_t stable_count;
+    static uint8_t stable_state;
+
+	static uint8_t raw_read()
+	{
+		static uint16_t limits[] = { 820, 475, 340, 175, 50 };
+		static uint8_t n_limits = sizeof(limits) / sizeof(*limits);
+		uint16_t x = adc::read<CHANNEL>();
+
+		for (uint8_t i = 0; i < n_limits; ++i)
+			if (x > limits[i])
+				return i;
+
+		return n_limits;
+	}
+};
+
+const uint8_t buttons_t::stable_count = 10;
+uint8_t buttons_t::stable_state = 0;
 
