658339872cfbbb698c93d2db4ad3abae30af062f
Bengt Marten Agren <maraingist@me.com>
2016-08-06 23:14:54 +0800

    Added shared main routine disabling of Leonardo USB interrupts when needed.

---
+++ AVR/Main.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <avr/io.h>
+
+void setup();
+void loop();
+
+int main()
+{
+
+#if defined(__AVR_ATmega32U4__)
+    // If this is leonardo the boot-loader leaves USB interrupts on
+    // and we'd crash on sei! So we turn off USB here...
+    USBCON &= ~_BV(USBE);
+#endif
+	setup();
+	for (;;)
+		loop();
+}
+
