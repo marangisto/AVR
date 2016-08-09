87a6362e080d96db0787b57024bdd3ed8795aed4
Bengt Marten Agren <maraingist@me.com>
2016-08-09 10:58:52 +0800

    Improved motor demo.

--- MotorShield23/Main.cpp
+++ MotorShield23/Main.cpp
@@ -1,78 +1,71 @@
 #include <AVR/TWI.h>
 #include <AVR/Main.h>
 #include <AVR/Delay.h>
-#include <Arduino/DFR0009.h>
+#include <Arduino/Pins.h>
 #include <Arduino/MotorShield23.h>
 
 typedef D13 LED;
-typedef dfr0009_t lcd;
-typedef buttons_t btns;
 typedef motor_shield_23_t<> ms23;
 static const int bridge = 0;
 
 void twi_error(uint8_t err, const char *file, uint16_t line)
 {
-    lcd::clear();
-    lcd::set_pos(0, 0);
-    lcd::write("TWI ERROR: 0x");
-    lcd::write(err, 16);
-    lcd::set_pos(1, 0);
-    lcd::write(file);
-    lcd::write(", ");
-    lcd::write(line);
-
-    while (true) ;  // stay here forever
+    while (true)
+    {
+        LED::toggle();
+        delay_ms(100);
+    }
 }
 
 void setup()
 {
 	LED::setup();
-    lcd::setup();
-    btns::setup();
     ms23::setup<0x1e>();  // keep last as it enables interrupts
+}
+
+static void update_motor()
+{
+    static int state = 0;
+    static int speed = 0;
+    static int step = 64;
+    static bool rev = false;
+
+    switch (state)
+    {
+    case 0:                 // initial state
+        speed = 0;
+        ms23::duty_cycle<bridge>(speed);
+        ms23::command<bridge>(rev ? ms23::reverse : ms23::forward);
+        rev = !rev;
+        state = 1;
+        break;
+    case 1:                 // accelerating
+        if (speed < 4096)
+        {
+            speed += step;
+            ms23::duty_cycle<bridge>(speed);
+        }
+        else
+            state = 2;
+        break;
+    case 2:                 // decelerating
+        if (speed > 0)
+        {
+            speed -= step;
+            ms23::duty_cycle<bridge>(speed);
+        }
+        else
+            state = 0;
+        break;
+    }
 }
 
 void loop()
 {
-    static uint8_t x = 0;
     static uint8_t i = 0;
-    static uint8_t err = 0;
 
-    bool update = false;
-
-    switch (btns::read())
-    {
-    case btn_up:
-        ++x;
-        update = true;
-        break;
-    case btn_down:
-        --x;
-        update = true;
-        break;
-    case btn_left:
-        ms23::command<bridge>(ms23::reverse);
-        break;
-    case btn_right:
-        ms23::command<bridge>(ms23::forward);
-        break;
-    case btn_select:
-        ms23::command<bridge>(ms23::stop);
-        break;
-    default: ;
-    }
-
-    if (update)
-    {
-        lcd::set_pos(0, 0);
-        lcd::write(x, 16);
-        lcd::write("  ");
-        lcd::set_pos(1, 0);
-        lcd::write(err, 16);
-        lcd::write("  ");
-
-        ms23::duty_cycle<bridge>(x << 4);
-    }
+    if ((i & 0xf) == 0)
+        update_motor();
 
     if (++i == 0)
 	    LED::toggle();
