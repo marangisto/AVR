edca91d6afe5457961d1740e01d10c53dfc10d9c
Bengt Marten Agren <maraingist@me.com>
2016-08-22 23:50:48 +0800

    Added cruise and deceleration phases for stepper.

--- Stepper/Main.cpp
+++ Stepper/Main.cpp
@@ -25,38 +25,69 @@ struct stepper_traits_t
     static uint16_t min_step_count() { return time_count(min_step_time()); }
     static float accel_to_max_speed_in_steps(uint16_t n) { return 2. / sqr(min_step_time() / (sqrt(n + 1) - sqrt(n))); }
     static inline float constant_acceleration_time(float accel, uint16_t l) { return sqrt(2. * l / accel); }
+    static uint16_t steps_to_max_speed(float accel) { return static_cast<uint16_t>(sqr(1. / min_step_time()) / (2. * accel)); }
 };
 
 typedef stepper_traits_t<200, 162> stepper_traits; // 17HS1003
 typedef timer_t<1> timer;
 
+enum stepper_state_t { ss_stop, ss_accel, ss_cruise, ss_decel };
+
+static volatile stepper_state_t s_state = ss_stop;
 static volatile float s_accel = stepper_traits::accel_to_max_speed_in_steps(200);
-static volatile uint16_t s_nsteps = 5000;
+static volatile uint16_t s_nsteps = 0;
+static volatile uint16_t s_nramp = 0;
 static volatile uint16_t s_step = 0;
-static volatile bool s_run = false;
-static volatile float s_time = .0;
 
 void isr()
 {
-    static const uint16_t min_step_count = stepper_traits::min_step_count();
-    if (!s_run)
+    static uint16_t step_count;
+    static float step_time, l_t;
+
+    if (s_state == ss_stop)
         return;
 
     STEP::set();
-    //delay_us(1);    // minumum 1us
 
-    float next_time = stepper_traits::constant_acceleration_time(s_accel, s_step + 1);
+    switch (s_state)
+    {
+    case ss_accel:
+        if (s_step == 0)
+            l_t = 0;
+        step_time = stepper_traits::constant_acceleration_time(s_accel, s_step + 1) - l_t;
+        step_count = stepper_traits::time_count(step_time);
+        l_t += step_time;
+        break;
+    case ss_cruise:
+        break;
+    case ss_decel:
+        if (s_step + s_nramp <= s_nsteps) // only true once
+            l_t = stepper_traits::constant_acceleration_time(s_accel, s_nsteps - s_step);
+        step_time = l_t - stepper_traits::constant_acceleration_time(s_accel, s_nsteps - s_step - 1);
+        step_count = stepper_traits::time_count(step_time);
+        l_t -= step_time;
+        break;
+    }
 
-    timer::counter() = 65535 - max(min_step_count, stepper_traits::time_count(next_time - s_time));
-
-    s_run = ++s_step < s_nsteps;
-    s_time = next_time;
-
+    delay_us(1);    // minumum 1us
     STEP::clear();
+
+    timer::counter() = 65535 - step_count;
+
+    if (++s_step < s_nramp)
+        s_state = ss_accel;
+    else if (s_step < s_nsteps - s_nramp)
+        s_state = ss_cruise;
+    else if (s_step < s_nsteps)
+        s_state = ss_decel;
+    else
+        s_state = ss_stop;
 }
 
 void run(uint16_t n, bool dir, float accel)
 {
+    printf("steps to max-speed = %d\n", stepper_traits::steps_to_max_speed(accel));
+
     printf("running %d steps %s...", n, dir ? "forward" : "reverse");
 
     DIR::write(dir);
@@ -64,11 +95,11 @@ void run(uint16_t n, bool dir, float accel)
 
     s_accel = accel;
     s_nsteps = n;
+    s_nramp = min(n / 2, stepper_traits::steps_to_max_speed(accel));
     s_step = 0;
-    s_time = .0;
-    s_run = true;
+    s_state = ss_accel;
 
-    while (s_run)
+    while (s_state != ss_stop)
         ;
 
     ENABLE::set();
@@ -110,7 +141,8 @@ void loop()
         last_x = x;
     }
 
-    run(x + 200, dir = !dir, stepper_traits::accel_to_max_speed_in_steps(x));
+    //run(x + 200, dir = !dir, stepper_traits::accel_to_max_speed_in_steps(x));
+    run(1000, dir = !dir, stepper_traits::accel_to_max_speed_in_steps(x));
     delay_ms(10);
 }
 
