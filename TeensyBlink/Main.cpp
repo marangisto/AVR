5b5df5c3211a9a891434167b4b76666f49c96e7a
Bengt Marten Agren <maraingist@me.com>
2016-11-01 22:41:44 +0800

    First raw port manipulation on ARM.

--- TeensyBlink/Main.cpp
+++ TeensyBlink/Main.cpp
@@ -1,19 +1,25 @@
-#include "WProgram.h"
+#include <kinetis.h>
 
-static uint8_t LED = 13;
+extern "C" void delay(uint32_t ms); // FIXME: figure out where this comes from
+
+// N.B. PORTC-PIN5 == LED 'Pin 13'
 
 extern "C"
 void setup()
 {
-    pinMode(LED, OUTPUT);
+    GPIOC_PDDR |= (1<<5);
+    PORTC_PCR5 = PORT_PCR_SRE | PORT_PCR_DSE | PORT_PCR_MUX(1);
 }
 
 extern "C"
 void loop()
 {
-    digitalWriteFast(LED, HIGH);
-    delay(50);
-    digitalWriteFast(LED, LOW);
+    //GPIOC_PSOR = (1<<5);  // SET
+    //delay(50);
+
+    //GPIOC_PCOR = (1<<5);  // CLEAR
+
+    GPIOC_PTOR = (1<<5);    // TOGGLE
     delay(500);
 }
 
