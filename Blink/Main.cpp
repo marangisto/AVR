bc3c90552e3c07d34bb92e2a1eceb87c9cf9571c
Bengt Marten Agren <maraingist@me.com>
2018-02-10 20:56:46 +0800

    Blinking an ATTiny-84.

--- Blink/Main.cpp
+++ Blink/Main.cpp
@@ -1,8 +1,13 @@
 #include <AVR/Main.h>
 #include <AVR/Delay.h>
-#include <Arduino/Pins.h>
 
-typedef D13 LED;
+#if defined(__AVR_ATtiny84__)
+    #include <AVR/Pins.h>
+    typedef output_t<PB, 2> LED;
+#else
+    #include <Arduino/Pins.h>
+    typedef D13 LED;
+#endif
 
 void setup()
 {
