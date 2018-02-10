f4d0c18869cfeba1241efd040279ab5f94cca8bb
Bengt Marten Agren <maraingist@me.com>
2018-02-10 22:51:35 +0800

    SPI support for tiny-84.

--- AVR/SPI.h
+++ AVR/SPI.h
@@ -16,6 +16,7 @@ struct spi_clock_traits
     static inline uint8_t spr10() { return 0; }
 };
 
+#if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega328__)
 template <>
 struct spi_clock_traits<4>
 {
@@ -58,9 +59,15 @@ struct spi_clock_traits<32>
     static inline uint8_t spi2x() { return _BV(SPI2X); }
     static inline uint8_t spr10() { return _BV(SPR1); }
 };
+#endif
 
+#if defined(__AVR_ATtiny84__)
+typedef output_t<PA, 5> MOSI;   // DO
+typedef output_t<PA, 4> SCK;
+#elif defined(__AVR_ATmega328P__) || defined(__AVR_ATmega328__)
 typedef output_t<PB, 3> MOSI;
 typedef output_t<PB, 5> SCK;
+#endif
 
 template<int CLOCK_DIV, spi_shift_order_t SHIFT_ORD, class PORT, unsigned BIT>
 struct spi_t
@@ -73,25 +80,47 @@ struct spi_t
         SCK::setup();
         CS::setup();
 
+#if defined(__AVR_ATtiny84__)
+        USICR = _BV(USIWM0) | _BV(USICS1) | _BV(USICLK);
+#elif defined(__AVR_ATmega328P__) || defined(__AVR_ATmega328__)
         SPCR = _BV(SPE) | _BV(MSTR) | (SHIFT_ORD<<DORD) | spi_clock_traits<CLOCK_DIV>::spr10();
         SPSR = spi_clock_traits<CLOCK_DIV>::spi2x();
+#endif
     }
 
     static void write(uint8_t x)
     {
         CS::clear();
+#if defined(__AVR_ATtiny84__)
+        USIDR = x;
+        USISR = _BV(USIOIF);
+        while (!(USISR & _BV(USIOIF)))
+            USICR |= _BV(USITC);
+#elif defined(__AVR_ATmega328P__) || defined(__AVR_ATmega328__)
         SPDR = x;
         loop_until_bit_is_set(SPSR, SPIF);
+#endif
         CS::set();
     }
 
     static void write(uint16_t x)
     {
         CS::clear();
+#if defined(__AVR_ATtiny84__)
+        USIDR = SHIFT_ORD ? (x & 0xff) : (x >> 8);
+        USISR = _BV(USIOIF);
+        while (!(USISR & _BV(USIOIF)))
+            USICR |= _BV(USITC);
+        USIDR = SHIFT_ORD ? (x >> 8) : (x & 0xff);
+        USISR = _BV(USIOIF);
+        while (!(USISR & _BV(USIOIF)))
+            USICR |= _BV(USITC);
+#elif defined(__AVR_ATmega328P__) || defined(__AVR_ATmega328__)
         SPDR = SHIFT_ORD ? (x & 0xff) : (x >> 8);
         loop_until_bit_is_set(SPSR, SPIF);
         SPDR = SHIFT_ORD ? (x >> 8) : (x & 0xff);
         loop_until_bit_is_set(SPSR, SPIF);
+#endif
         CS::set();
     }
 };
