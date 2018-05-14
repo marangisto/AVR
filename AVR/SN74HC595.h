74f49001d2654bd9b0b9d79b22982465c3d3ba1b
Bengt Marten Agren <maraingist@me.com>
2018-05-14 23:39:25 +0800

    Added 16 bit support for cascading 74HC595 shift-registers.

--- AVR/SN74HC595.h
+++ AVR/SN74HC595.h
@@ -12,6 +12,14 @@ static inline void clock_out_bit(uint8_t x, int i)
     CK::set();
 }
 
+template<class DT, class CK>
+static inline void clock_out_bit16(uint16_t x, int i)
+{
+    CK::clear();
+    DT::write(((x >> i) & 0x01) != 0);
+    CK::set();
+}
+
 template<class DT, class CK, shift_order_t SD>
 struct shift_out_impl
 {
@@ -19,6 +27,12 @@ struct shift_out_impl
     {
         for (int8_t i = 7; i >= 0; --i)
             clock_out_bit<DT, CK>(x, i);
+    }
+
+    static void shift_out16(uint16_t x)
+    {
+        for (int8_t i = 15; i >= 0; --i)
+            clock_out_bit16<DT, CK>(x, i);
     }
 };
 
@@ -30,12 +44,24 @@ struct shift_out_impl<DT, CK, LSB_FIRST>
         for (int8_t i = 0; i < 8; ++i)
             clock_out_bit<DT, CK>(x, i);
     }
+
+    static void shift_out16(uint16_t x)
+    {
+        for (int8_t i = 0; i < 16; ++i)
+            clock_out_bit16<DT, CK>(x, i);
+    }
 };
 
 template<class DT, class CK, shift_order_t SD>
 void shift_out(uint8_t x)
 {
     shift_out_impl<DT, CK, SD>::shift_out(x);
+}
+
+template<class DT, class CK, shift_order_t SD>
+void shift_out16(uint16_t x)
+{
+    shift_out_impl<DT, CK, SD>::shift_out16(x);
 }
 
 template<class DT, class CK, class LT, shift_order_t SD>
@@ -52,6 +78,13 @@ struct sn74hc595_t
     {
         LT::clear();
         shift_out<DT, CK, SD>(x);
+        LT::set();
+    }
+
+    static void write(uint16_t x)
+    {
+        LT::clear();
+        shift_out16<DT, CK, SD>(x);
         LT::set();
     }
 };
