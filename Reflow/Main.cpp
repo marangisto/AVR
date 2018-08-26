4a70e573024f349d948a84e2b3218f711dc07743
Bengt Marten Agren <maraingist@me.com>
2018-08-26 15:39:22 +0800

    Added slow-pwm control.

--- Reflow/Main.cpp
+++ Reflow/Main.cpp
@@ -7,9 +7,36 @@
 #include <string.h>
 #include <stdlib.h>
 
+template <class T> const T& max(const T& a, const T& b) { return (a<b) ? b : a; }
+template <class T> const T& min(const T& a, const T& b) { return (a<b) ? a : b; }
+
 typedef D13 led;
 typedef D3 ssr_a;
 typedef D2 ssr_b;
+
+template<class PIN>
+class slow_pwm
+{
+public:
+    static void update()
+    {
+        PIN::write(m_x && m_x >= m_i++);
+    }
+
+    static void set(float x)    // [0..1] range
+    {
+        m_x = static_cast<uint8_t>(min(max(.0, 255. * x), 255.));
+    }
+
+private:
+    static volatile uint8_t m_x;
+    static uint8_t m_i;
+};
+
+template<class PIN> volatile uint8_t slow_pwm<PIN>::m_x = 0;
+template<class PIN> uint8_t slow_pwm<PIN>::m_i = 0;
+
+typedef slow_pwm<ssr_a> pwm_a;
 
 typedef timer_t<0> clock;
 
@@ -23,6 +50,7 @@ static void clock_isr()
         ++clock_ticks;
 
     tick_tock = !tick_tock;
+    pwm_a::update();
 }
 
 static float clock_time(uint16_t ticks) { return 20e-3 * ticks; }
@@ -95,6 +123,8 @@ static void control_isr()
 
     temp_a = v_a / 5e-3;
     temp_b = v_b / 5e-3;
+
+    pwm_a::set(0.5);
 }
 
 void setup()
@@ -116,7 +146,7 @@ void setup()
     clock::clock_select<1024>();
     clock::output_pin<channel_a>::setup();
     clock::compare_output_mode<channel_a, toggle_on_compare_match>();
-    clock::output_compare_register<channel_a>() = 155; // 20ms period
+    clock::output_compare_register<channel_a>() = 155; // 10ms period
     clock::isr_oca(clock_isr);
     clock::enable_oca();
 
@@ -127,8 +157,6 @@ void loop()
 {
     static uint8_t i = 0;
 
-    if ((i & 0x3) == 0)
-        ssr_a::toggle();
     if ((i & 0x5) == 0)
         ssr_b::toggle();
 
