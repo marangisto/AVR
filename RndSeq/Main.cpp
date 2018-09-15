0b825cc4bc5c7ac215c7ac1a6523279752a63565
Bengt Marten Agren <maraingist@me.com>
2018-09-15 17:50:47 +0800

    Clock input and sequence memory.

--- RndSeq/Main.cpp
+++ RndSeq/Main.cpp
@@ -18,18 +18,61 @@ typedef output_t<PD, 7> led_trig;
 
 typedef outputs_t<led_0, led_1, led_2, led_3> led_0123;
 
-typedef input_t<PB, 0, enable_pullup> clk;
+typedef input_t<PB, 0> clk;
 
 typedef timer_t<1> pwm;
 typedef timer_t<2> aux;
 
 static volatile uint8_t display = 0;
 
+static uint16_t notes[] =
+    { 0, 18, 37, 55, 73, 91, 110, 128, 146, 164, 183, 201, 219, 238, 256
+    , 274, 292, 311, 329, 347, 365, 384, 402, 420, 439, 457, 475, 493, 512
+    , 530, 548, 566, 585, 603, 621, 640, 658, 676, 694, 713, 731, 749, 767
+    , 786, 804, 822, 841, 859, 877, 895, 914, 932, 950, 968, 987, 1005
+    , 1023, 1042, 1060, 1078, 1096
+    };
+
+static const uint8_t n_notes = sizeof(notes) / sizeof(*notes);
+
+static volatile uint16_t sequence[256] = { 0, 2, 4, 3, 6, 7, 12, 5 };
+
+static volatile uint8_t n_steps = 8;
+
+static void step()
+{
+    static uint8_t i = 0;
+    uint8_t note = sequence[i];
+    uint16_t duty = notes[min(note, n_notes)];
+
+    pwm::output_compare_register<channel_a>() = 0x3ff - duty;   // inverted output
+    trig::clear();
+    delay_ms(10);
+    trig::set();
+    display = i;
+    i = ++i < n_steps ? i : 0;
+}
+
 ISR(TIMER2_OVF_vect)
 {
     static uint8_t i = 0;
 
-    led_0123::write(i++ < 10 ? display : 0);
+    led_0123::write(i++ < 15 ? display : 0);
+}
+
+ISR(PCINT0_vect)
+{
+    static bool last_clk = false;
+
+    bool c = clk::read();
+
+    if (c != last_clk)
+    {
+        led_trig::write(c);
+        if (c)
+            step();
+        last_clk = c;
+    }
 }
 
 void setup()
@@ -43,11 +86,10 @@ void setup()
     clk::setup();
     adc::setup<128>();
 
-    const wg_mode mode = pwm_phase_correct;
-    const int prescale = 1;
+    trig::set();      // because output buffer is inverted
 
-    pwm::setup<mode, top_0xff>();
-    pwm::clock_select<prescale>();
+    pwm::setup<pwm_phase_correct, top_0x3ff>();
+    pwm::clock_select<1>();
     pwm::output_pin<channel_a>::setup();
     pwm::compare_output_mode<channel_a, clear_on_compare_match>();
 
@@ -55,27 +97,25 @@ void setup()
     aux::clock_select<1>();
     aux::enable();
 
-    /*
     PCMSK0 |= _BV(PCINT0);
     PCICR |= _BV(PCIE0);
-    */
+
     sei();
 }
 
 void loop()
 {
-    static uint8_t i = 0;
-
-    pwm::output_compare_register<channel_a>() = 255 - (i % 12);
-    trig::set();
+    /*
+    pwm::output_compare_register<channel_a>() = 0x3ff - notes[i % 12];
+    trig::clear();      // because output buffer is inverted
     led_trig::set();
-    delay_ms(10);
-    trig::clear();
+    delay_ms(1);
+    trig::set();
     led_trig::clear();
 
     display = i;
+    */
+//    step();
     delay_ms(100);
-
-    i += 1;
 }
 
