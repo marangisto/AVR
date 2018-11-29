c0ab5e54deadd5fae1187e19b96a9ed0fce112e4
Bengt Marten Agren <maraingist@me.com>
2018-11-29 22:45:55 +0800

    Added start / stop toggle and single-step.

--- Sequence/Main.cpp
+++ Sequence/Main.cpp
@@ -11,6 +11,9 @@ typedef D13 LED;
 typedef output_t<PD, 2> trig_a;
 typedef output_t<PD, 3> trig_b;
 
+typedef input_t<PD, 4, enable_pullup> btn_a;
+typedef input_t<PD, 5, enable_pullup> btn_b;
+
 typedef twi_master_t<0> twi;
 
 static uint8_t twi_addr = 0;
@@ -27,13 +30,14 @@ typedef timer_t<2> aux;
 
 static const uint16_t aux_prescale = 64;
 static volatile uint16_t aux_count = 0;
+static volatile bool auto_step = true;
 static volatile bool do_step = false;
 
 ISR(TIMER2_OVF_vect)
 {
     static uint16_t i = 0;
 
-    if (i++ >= aux_count)
+    if (auto_step && i++ >= aux_count)
     {
         do_step = true;
         i = 0;
@@ -47,6 +51,8 @@ void setup()
     adc::setup<128>();
     trig_a::setup();
     trig_b::setup();
+    btn_a::setup();
+    btn_b::setup();
 
     pwm::setup<fast_pwm, top_0x1ff>();
     pwm::clock_select<1>();
@@ -81,8 +87,22 @@ void setup()
 void loop()
 {
     static uint8_t i = 0;
+    static bool last_btn_a = true;
+    static bool last_btn_b = true;
 
     aux_count = 1 + adc::read<adc_bpm>();
+
+    bool b = btn_a::read();
+
+    if (b != last_btn_a && !b)                      // run / stop toggle
+            auto_step = !auto_step;
+    last_btn_a = b;
+
+    b = btn_b::read();
+
+    if (b != last_btn_b && !b)                      // run / stop toggle
+            do_step = true;
+    last_btn_b = b;
 
     if (do_step)
     {
@@ -107,14 +127,14 @@ void loop()
         printf("%d %d %s\n", i, value, sw_a ? "a" : (sw_b ? "b" : " "));
         if (sw_a )
         {
-            pwm::output_compare_register<channel_a>() = /* 0x1ff - */ (value >> 1);   // inverted output
+            pwm::output_compare_register<channel_a>() = /* 0x1ff - */ (value >> 2);   // inverted output
             trig_a::set();
             delay_us(100);
             trig_a::clear();
         }
         else if (sw_b)
         {
-            pwm::output_compare_register<channel_b>() = /* 0x1ff - */ (value >> 1);   // inverted output
+            pwm::output_compare_register<channel_b>() = /* 0x1ff - */ (value >> 2);   // inverted output
             trig_b::set();
             delay_us(100);
             trig_b::clear();
