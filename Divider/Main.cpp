9747b67eaccbda6584b775bd338ddd1251d9131a
Bengt Marten Agren <maraingist@me.com>
2019-02-19 00:33:36 +0800

    Interrupts and proper output ordering.

--- Divider/Main.cpp
+++ Divider/Main.cpp
@@ -1,25 +1,43 @@
 #define NO_TIMER_VECTORS 1
 #include <AVR/Main.h>
 #include <AVR/Delay.h>
+#include <AVR/Timer.h>
 #include <AVR/ADC.h>
 #include <AVR/Pins.h>
 
 template <class T> const T& max(const T& a, const T& b) { return (a<b) ? b : a; }
 template <class T> const T& min(const T& a, const T& b) { return (a<b) ? a : b; }
 
-typedef output_t<PB, 2> out_0;
-typedef output_t<PA, 2> out_1;
-typedef output_t<PB, 0> out_2;
-typedef output_t<PA, 1> out_3;
-typedef output_t<PA, 4> out_4;
-typedef output_t<PA, 5> out_5;
-typedef output_t<PA, 6> out_6;
-typedef output_t<PA, 0> out_7;
+typedef output_t<PB, 2> out_0; // 0
+typedef output_t<PA, 2> out_4; // 4
+typedef output_t<PB, 0> out_1; // 1
+typedef output_t<PA, 1> out_5; // 5
+typedef output_t<PA, 4> out_2; // 2
+typedef output_t<PA, 5> out_6; // 6
+typedef output_t<PA, 6> out_3; // 3
+typedef output_t<PA, 0> out_7; // 7
 
-typedef input_t<PA, 3> in_rst;
-typedef input_t<PB, 1> in_clk;
+typedef input_t<PA, 3> in_rst;  // PCINT3
+typedef input_t<PB, 1> in_clk;  // PCINT9
 
 static const uint8_t spdts = 7;
+
+static volatile bool clk_rising = false;
+static volatile bool clk_falling = false;
+
+ISR(PCINT0_vect)
+{
+    if (!in_rst::read())    // inverted input
+        ; // FIXME: perform reset here
+}
+
+ISR(PCINT1_vect)
+{
+    if (!in_clk::read())    // inverted input
+        clk_rising = true;
+    else
+        clk_falling = true;
+}
 
 void setup()
 {
@@ -35,9 +53,16 @@ void setup()
     in_clk::setup();
     adc::setup<128>();
 
-    // sei();
+    PCMSK0 |= _BV(PCINT3);  // enable pin-change interrupt on rst (PCINT3)
+    GIMSK |= _BV(PCIE0);    // enable channel 0 pin-change interrupts
+
+    PCMSK1 |= _BV(PCINT9);  // enable pin-change interrupt on clk (PCINT9)
+    GIMSK |= _BV(PCIE1);    // enable channel 1 pin-change interrupts
+
+    sei();
 }
 
+/*
 static bool update(uint8_t& c, uint8_t m)
 {
     if (c++ >= m)
@@ -48,9 +73,11 @@ static bool update(uint8_t& c, uint8_t m)
 
     return false;
 }
+*/
 
 void loop()
 {
+    /*
     static uint32_t clock = 0;
     static bool tick = false;
     static bool b0 = false;
@@ -61,7 +88,6 @@ void loop()
     static bool b5 = false;
     static bool b6 = false;
     static bool b7 = false;
-    static uint8_t i = 0;
     static uint8_t m1 = 2;
     static uint8_t m2 = 4;
     static uint8_t m3 = 8;
@@ -106,7 +132,36 @@ void loop()
     if (tick)
         ++clock;
 
-    tick = false;
-    delay_ms(1);
+    */
+    static uint8_t i = 0;
+
+    if (clk_rising)
+    {
+        out_0::set();
+        clk_rising = false;
+        i = (i + 1) & 0x7;
+        switch (i)
+        {
+            case 1: out_1::set(); break;
+            case 2: out_2::set(); break;
+            case 3: out_3::set(); break;
+            case 4: out_4::set(); break;
+            case 5: out_5::set(); break;
+            case 6: out_6::set(); break;
+            case 7: out_7::set(); break;
+        }
+    }
+    else if (clk_falling)
+    {
+        out_0::clear();
+        clk_falling = false;
+        out_1::clear();
+        out_2::clear();
+        out_3::clear();
+        out_4::clear();
+        out_5::clear();
+        out_6::clear();
+        out_7::clear();
+    }
 }
 
