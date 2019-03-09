a2c02964fddf7fc79e92e5ed0c64471a55b923d9
Bengt Marten Agren <maraingist@me.com>
2019-03-09 14:10:56 +0800

    Implemented sine waveform.

--- LFO/Main.cpp
+++ LFO/Main.cpp
@@ -46,6 +46,7 @@ static uint8_t read_spdts() // return sw0 | (sw1 << 2)
     return sw_err | (sw_err << 2);
 }
 
+/*
 static void show(uint16_t x)
 {
     out_trig::clear();
@@ -60,19 +61,27 @@ static void show(uint16_t x)
         delay_ms(1);
     }
 }
+*/
 
-enum waveform_t { wf_tri, wf_saw, wf_sin };
-
-static volatile waveform_t waveform = wf_tri;
-
-enum state_t { reset, tri_up, tri_dn, saw_up };
+enum state_t { reset, tri_up, tri_dn, saw_up, sin_a, sin_b, sin_c, sin_d };
 
 static volatile state_t start = tri_up;
 static volatile state_t state = reset;
 
+static const uint8_t sin_tab[] =
+    { 0,2,3,5,6,8,9,11,13,14,16,17,19,20,22,23,25,27,28,30,31,33,34,36,37,39,41,42,44,45,47,48
+    , 50,51,53,54,56,57,59,60,62,63,65,67,68,70,71,73,74,76,77,79,80,81,83,84,86,87,89,90,92,93,95,96
+    , 98,99,100,102,103,105,106,108,109,110,112,113,115,116,117,119,120,122,123,124,126,127,128,130,131,132,134,135,136,138,139,140
+    , 142,143,144,146,147,148,149,151,152,153,154,156,157,158,159,161,162,163,164,165,167,168,169,170,171,172,174,175,176,177,178,179
+    , 180,181,183,184,185,186,187,188,189,190,191,192,193,194,195,196,197,198,199,200,201,202,203,204,205,206,207,208,208,209,210,211
+    , 212,213,214,215,215,216,217,218,219,220,220,221,222,223,223,224,225,226,226,227,228,228,229,230,231,231,232,232,233,234,234,235
+    , 236,236,237,237,238,238,239,240,240,241,241,242,242,243,243,244,244,244,245,245,246,246,247,247,247,248,248,248,249,249,249,250
+    , 250,250,251,251,251,252,252,252,252,252,253,253,253,253,253,254,254,254,254,254,254,254,255,255,255,255,255,255,255,255,255,255
+    };
+
 ISR(TIM0_COMPA_vect)
 {
-    static uint16_t i = 0;
+    static int16_t i = 0;
     int16_t y = 0;
 
     if (state == reset)
@@ -98,6 +107,36 @@ ISR(TIM0_COMPA_vect)
     case saw_up:
         y = i >> 1;
         break;
+    case sin_a:
+        if (i < 256)
+        {
+            y = 256 + static_cast<int16_t>(sin_tab[i]);
+            break;
+        }
+        else
+            state = sin_b;
+        // fall through
+    case sin_b:
+        if (i < 512)
+        {
+            y = 256 + static_cast<int16_t>(sin_tab[256-(i-255)]);
+            break;
+        }
+        else
+            state = sin_c;
+        // fall through
+    case sin_c:
+        if (i < 768)
+        {
+            y = 256 - static_cast<int16_t>(sin_tab[i-512]);
+            break;
+        }
+        else
+            state = sin_d;
+        // fall through
+    case sin_d:
+        y = 256 - static_cast<int16_t>(sin_tab[256-(i-767)]);
+        break;
     default: ;
     }
 
@@ -120,9 +159,6 @@ void setup()
     pwm::output_pin<channel_a>::setup();
     pwm::compare_output_mode<channel_a, clear_on_compare_match>();
 
-    read_spdts();
-    show(adc::read<adc_pwm>());
- 
     time::setup<ctc_mode, top_ocra>();
     time::clock_select<8>();
     time::output_compare_register<channel_a>() = 50;
@@ -156,14 +192,13 @@ void loop()
         sw1 = _sw1;
 
         cli();
-
         switch (sw1)
         {
-            case sw_up: start = tri_up; state = reset; break;
-            case sw_mid: start = saw_up; state = reset; break;
+            case sw_up: start = saw_up; state = reset; break;
+            case sw_mid: start = tri_up; state = reset; break;
+            case sw_dn: start = sin_a; state = reset; break;
             default: ;
         }
-
         sei();
     }
     //out_led::toggle();
